7ff7072bd4282671a79b5c89ec3ce999e14317bb
nitaifingerhut <nitaifingerhut@gmail.com>
2021-05-24 19:04:52 +0300

    modifying scripts

--- scripts/main_0.sh
+++ scripts/main_0.sh
@@ -8,7 +8,7 @@ echo "hello from $(python --version) in $(which python)"
 echo "(nlp_hw1) training starts"
 
 # Run the experiments
-python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --epochs 100 --reg-lambda 0.01 --optimizer BatchSGD --batch-size 256 --features-params pairs=50 unigrams=50 bigrams=250 trigrams=500 prefixes=50 suffixes=50 prev_w_curr_t=50 next_w_curr_t=50
+python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --epochs 250 --reg-lambda 0.01 --features-params pairs=50 unigrams=50 bigrams=250 trigrams=500 prefixes=50 suffixes=50 prev_w_curr_t=50 next_w_curr_t=50
 git add -A
 git commit -m "from Newton with love"
 git push
--- scripts/main_1.sh
+++ scripts/main_1.sh
@@ -8,19 +8,7 @@ echo "hello from $(python --version) in $(which python)"
 echo "(nlp_hw1) training starts"
 
 # Run the experiments
-python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --n-pairs 500 --n-unigrams 500 --n-bigrams 1000 --n-trigrams 1500 --n-prefixes 500 --n-suffixes 500 --n-prev-w-curr-t 500 --n-next-w-curr-t 500 --epochs 10000 --reg-lambda 0.001 --optimizer BatchSGD --batch-size 64
+python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --epochs 250 --reg-lambda 0.01 --features-params pairs=250 unigrams=50 bigrams=250 trigrams=500 prefixes=100 suffixes=100 prev_w_curr_t=100 next_w_curr_t=100
 git add -A
-git commit -m "auto... BatchSGD"
-git push
-python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --n-pairs 500 --n-unigrams 500 --n-bigrams 1000 --n-trigrams 1500 --n-prefixes 500 --n-suffixes 500 --n-prev-w-curr-t 500 --n-next-w-curr-t 500 --epochs 10000 --reg-lambda 0.001 --optimizer BatchSGD --batch-size 128
-git add -A
-git commit -m "auto... BatchSGD"
-git push
-python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --n-pairs 500 --n-unigrams 500 --n-bigrams 1000 --n-trigrams 1500 --n-prefixes 500 --n-suffixes 500 --n-prev-w-curr-t 500 --n-next-w-curr-t 500 --epochs 10000 --reg-lambda 0.001 --optimizer BatchSGD --batch-size 256
-git add -A
-git commit -m "auto... BatchSGD"
-git push
-python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --n-pairs 500 --n-unigrams 500 --n-bigrams 1000 --n-trigrams 1500 --n-prefixes 500 --n-suffixes 500 --n-prev-w-curr-t 500 --n-next-w-curr-t 500 --epochs 10000 --reg-lambda 0.001 --optimizer BatchSGD --batch-size 512
-git add -A
-git commit -m "auto... BatchSGD"
+git commit -m "from Newton with love"
 git push
