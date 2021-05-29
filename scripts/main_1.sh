84acbf7d2d285c2560e83eb18e046f0c4fd705fd
daniel-ablin <daniel.avlin@gmail.com>
2021-05-29 12:49:56 +0300

    Revert "Merge branch 'daniel-v2' of https://github.com/nitaifingerhut/memm-pos-tagger into daniel-v2"
    This reverts commit 75627f15183c039dc005b05ae32ea0df1d609a49, reversing
    changes made to fd759ed03a6302e24af30ce184517b959917b27d.

--- scripts/main_1.sh
+++ scripts/main_1.sh
@@ -8,23 +8,7 @@ echo "hello from $(python --version) in $(which python)"
 echo "(nlp_hw1) training starts"
 
 # Run the experiments
-python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --epochs 5000 --reg-lambda 1 --beam 3 --name mid_l_1_b_3 --features-params pairs=250 unigrams=50 bigrams=500 trigrams=1000 prefixes=250 suffixes=250 prev_w_curr_t=250 next_w_curr_t=250 index_tag=250 index_word=250
+python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --epochs 250 --reg-lambda 0.01 --features-params pairs=250 unigrams=50 bigrams=250 trigrams=500 prefixes=100 suffixes=100 prev_w_curr_t=100 next_w_curr_t=100
 git add -A
 git commit -m "from Newton with love"
-git push origin daniel-v2
-python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --epochs 5000 --reg-lambda 0.1 --beam 3 --name mid_l_0.1_b_3 --features-params pairs=250 unigrams=50 bigrams=500 trigrams=1000 prefixes=250 suffixes=250 prev_w_curr_t=250 next_w_curr_t=250 index_tag=250 index_word=250
-git add -A
-git commit -m "from Newton with love"
-git push origin daniel-v2
-python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --epochs 5000 --reg-lambda 0.01 --beam 3 --name mid_l_0.01_b_3 --features-params pairs=250 unigrams=50 bigrams=500 trigrams=1000 prefixes=250 suffixes=250 prev_w_curr_t=250 next_w_curr_t=250 index_tag=250 index_word=250
-git add -A
-git commit -m "from Newton with love"
-git push origin daniel-v2
-python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --epochs 5000 --reg-lambda 0.001 --beam 3 --name mid_l_0.001_b_3 --features-params pairs=250 unigrams=50 bigrams=500 trigrams=1000 prefixes=250 suffixes=250 prev_w_curr_t=250 next_w_curr_t=250 index_tag=250 index_word=250
-git add -A
-git commit -m "from Newton with love"
-git push origin daniel-v2
-python main.py --train-file assets/train1.wtag --test-file assets/test1.wtag --epochs 5000 --reg-lambda 0.0001 --beam 3 --name mid_l_0.0001_b_3 --features-params pairs=250 unigrams=50 bigrams=500 trigrams=1000 prefixes=250 suffixes=250 prev_w_curr_t=250 next_w_curr_t=250 index_tag=250 index_word=250
-git add -A
-git commit -m "from Newton with love"
-git push origin daniel-v2
+git push
