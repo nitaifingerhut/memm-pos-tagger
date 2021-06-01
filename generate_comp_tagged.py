999a9b8ef0238b80a627033276e23250a8eee8ca
Nitai Fingerhut <nitai@Nitais-MacBook-Pro-3.local>
2021-06-01 20:41:05 +0300

    finally

--- generate_comp_tagged.py
+++ generate_comp_tagged.py
@@ -6,7 +6,20 @@ from pos_tagger.memm import MEMM
 from utils.parser import Parser
 
 
-OPTIMAL_MODEL_PATH = Path("models/final_2").absolute()
+INFO = {
+    "comp1": {
+        "params": Path("models/final_2/params.json").absolute(),
+        "weights": Path("models/final_2/trained_weights_data_1.pkl").absolute(),
+        "src_path": Path("assets/comp1.words").absolute(),
+        "trg_path": Path("comp_m1_302919402.wtag").absolute(),
+    },
+    "comp2": {
+        "params": Path("models/final_2_small/params.json").absolute(),
+        "weights": Path("models/final_2_small/trained_weights_data_2.pkl").absolute(),
+        "src_path": Path("assets/comp2.words").absolute(),
+        "trg_path": Path("comp_m2_302919402.wtag").absolute(),
+    }
+}
 
 
 if __name__ == "__main__":
@@ -17,24 +30,20 @@ if __name__ == "__main__":
     root_dir = Path("models").joinpath(opts.name).expanduser()
     root_dir.mkdir(parents=True, exist_ok=True)
 
-    features_path = OPTIMAL_MODEL_PATH.joinpath("params.json")
+    features_path = INFO[opts.comp_file_name]["params"]
     with open(features_path, "r") as f:
         data = json.load(f)
     opts.features_params = data["features_params"]
 
     memm = MEMM(opts, root_dir)
 
-    weights_path = OPTIMAL_MODEL_PATH.joinpath("trained_weights_data_1.pkl")
+    weights_path = INFO[opts.comp_file_name]["weights"]
     with open(weights_path, "rb") as f:
-            optimal_params = pickle.load(f)
+        optimal_params = pickle.load(f)
     memm.weights = optimal_params[0]
 
-    predictions = memm.predict_no_gt(Path("assets/comp1.words").absolute(), opts.beam)
-    w_path = Path.cwd().joinpath("comp_m1_302919402.wtag")
-    with open(w_path, "w") as f:
-        f.writelines("\n".join(predictions))
+    predictions = memm.predict_no_gt(INFO[opts.comp_file_name]["src_path"], opts.beam)
 
-    predictions = memm.predict_no_gt(Path("assets/comp2.words").absolute(), opts.beam)
-    w_path = Path.cwd().joinpath("comp_m2_302919402.wtag")
+    w_path = INFO[opts.comp_file_name]["trg_path"]
     with open(w_path, "w") as f:
         f.writelines("\n".join(predictions))
--- main.py
+++ main.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
 
     memm = MEMM(opts, root_dir)
     memm.fit(opts=opts)
-    _, opts.test_accuracy = memm.predict(opts.test_file, opts.beam)
+    ## Remove when running on test with ground truth
+    # _, opts.test_accuracy = memm.predict(opts.test_file, opts.beam)
 
     w_path = root_dir.joinpath("params.json")
     with open(w_path, "w") as f:
@@ -22,5 +23,5 @@ if __name__ == "__main__":
         opts.test_file = opts.test_file.as_posix()
         json.dump(vars(opts), f, indent=4)
 
-    memm.predict_no_gt(Path("assets/comp1.words").absolute(), opts.beam)
-    memm.predict_no_gt(Path("assets/comp2.words").absolute(), opts.beam)
+    # memm.predict_no_gt(Path("assets/comp1.words").absolute(), opts.beam)
+    # memm.predict_no_gt(Path("assets/comp2.words").absolute(), opts.beam)
